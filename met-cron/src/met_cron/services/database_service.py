31fe133b77cdda2b3590714a8579005e0c0df471
Shaelyn Tolkamp <46355612+tolkamps1@users.noreply.github.com>
2026-04-07 10:12:29 -0700

    [Engage-144-3] met-cron purge job (#200)

--- met-cron/src/met_cron/services/database_service.py
+++ met-cron/src/met_cron/services/database_service.py
@@ -22,9 +22,12 @@ class DatabaseService:  # pylint: disable=too-few-public-methods
     @staticmethod
     def purge_event_logs():
         """Purge dagster event logs. Keep only last 7 days."""
-        with db.engine.connect() as con:
+
+        with db.engine.begin() as con:
             con.execute(text("""
                 DELETE FROM dagster.event_logs
                 WHERE timestamp < current_timestamp - interval '7 days';
             """))
-            con.execution_options(isolation_level="AUTOCOMMIT").execute(text('VACUUM dagster.event_logs;'))
+
+        with db.engine.connect().execution_options(isolation_level="AUTOCOMMIT") as con:
+            con.execute(text("VACUUM dagster.event_logs;"))
